4896062cfe521ff41aaec31e84876c93ff845fb1
chaebeen <coqls0531@naver.com>
2025-10-14 21:22:18 +0900

    fix(Application.main): 문자열 입력 시 Scanner에서 Console API 사용으로 변경
    문자열 입력 기능을 구현할 때, 요구사항에 따라 Scanner API 사용에서 Console API 사용으로 변경함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,14 +1,12 @@
 package calculator;
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
         System.out.println("덧셈할 문자열을 입력해주세요.");
 
-        String input = sc.nextLine();
+        String input = Console.readLine();
 
         int result = 0;
 
