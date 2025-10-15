c02f585d5ed775b6f5e7daaa0653a08dce333fbd
beanbeen0 <coqls0531@naver.com>
2025-10-16 02:10:31 +0900

    feat(Application.java): 커스텀 구분자 지정 기능 추가
    - Pattern과 Matcher를 사용해 커스텀 구분자와 데이터를 추출 후 지정
    - Pattern.quote()로 특수문자 구분자 안전 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,16 +1,25 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
 
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();
+        String data = input;
         String delimiter = ",|:";
 
+        Pattern customDelimeterPattern = Pattern.compile("^//(?<customDelimiter>.)\\\\n(?<data>.*)");
+        Matcher matcher = customDelimeterPattern.matcher(input);
+
+        if (matcher.matches()) {
+            delimiter = Pattern.quote(matcher.group("customDelimiter"));
+            data = matcher.group("data");
+        }
+
         int result = 0;
-
-
         String[] tokens = data.split(delimiter);
         for(String token : tokens) {
             if (token.equals("")) continue;
