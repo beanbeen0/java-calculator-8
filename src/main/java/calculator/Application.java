bb904c88beca6edafd3262cc82e32bdf3ec2abbf
beanbeen0 <coqls0531@naver.com>
2025-10-17 22:55:02 +0900

    chore: 주석 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,13 +4,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Application {
+
     public static void main(String[] args) {
 
+        // 입력 받기
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();
+
+        // 변수 초기화
         String data = input;
         String delimiter = ",|:";
 
+        // 커스텀 지정자 지정 기능
         Pattern customDelimeterPattern = Pattern.compile("^//(?<customDelimiter>.)\\\\n(?<data>.*)");
         Matcher matcher = customDelimeterPattern.matcher(input);
 
@@ -19,6 +24,7 @@ public class Application {
             data = matcher.group("data");
         }
 
+        // 구분자 기준으로 토큰 분리 후 정수화 -> 덧셈
         int result = 0;
         String[] tokens = data.split(delimiter);
         for(String token : tokens) {
@@ -28,6 +34,7 @@ public class Application {
             result += operand;
         }
 
+        //결과 출력
         System.out.println("결과 : " + result);
     }
 }
